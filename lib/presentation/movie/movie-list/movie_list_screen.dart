6207f9d2bcf9d9fc5d91b5ce23a6203aab84c92d
Buğra Göksu <bugra.goksu@parasut.com>
2022-07-22 20:21:14 +0300

    edit movie card

--- lib/presentation/movie/movie-list/movie_list_screen.dart
+++ lib/presentation/movie/movie-list/movie_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_challenge_project/presentation/movie/movie-detail/movie_detail_screen.dart';
 
 class MovieListScreen extends StatefulWidget {
   const MovieListScreen({Key? key}) : super(key: key);
@@ -12,35 +13,44 @@ class _MovieListScreenState extends State<MovieListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const TextField(
-                decoration: InputDecoration(
-                  label: Text('Search'),
+      appBar: AppBar(
+        title: const Text('Movies'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                label: Text('Search'),
+              ),
+            ),
+            const Spacer(),
+            Expanded(
+              flex: 12,
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 3 / 4,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) => _MovieCard(
+                  imageUrl:
+                      'https://kbimages1-a.akamaihd.net/538b1473-6d45-47f4-b16e-32a0a6ba7f9a/1200/1200/False/star-wars-episode-iv-a-new-hope-3.jpg',
+                  score: '7.3',
+                  title: 'Star Wars : A New Hope',
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: ((context) => const MovieDetailScreen(movieId: '1')),
+                      ),
+                    );
+                  },
                 ),
               ),
-              const Spacer(),
-              Expanded(
-                flex: 12,
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    childAspectRatio: 3 / 4,
-                    mainAxisSpacing: 10,
-                  ),
-                  itemCount: 10,
-                  itemBuilder: (BuildContext context, int index) => const _MovieCard(
-                    imageUrl: 'https://via.placeholder.com/500',
-                    score: '7.3',
-                    title: 'Star Wars : A New Hope',
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -53,63 +63,79 @@ class _MovieCard extends StatelessWidget {
     required this.imageUrl,
     required this.score,
     required this.title,
+    required this.onTap,
   }) : super(key: key);
 
+  final VoidCallback onTap;
   final String imageUrl;
   final String score;
   final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Stack(
-        children: [
-          CachedNetworkImage(
-            imageUrl: imageUrl,
-            errorWidget: (context, url, error) => const Placeholder(),
-            fit: BoxFit.cover,
-          ),
-          Positioned.fill(
-            bottom: 10,
-            left: 10,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                DecoratedBox(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black,
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            CachedNetworkImage(
+              imageUrl: imageUrl,
+              errorWidget: (context, url, error) => const Placeholder(),
+              fit: BoxFit.cover,
+            ),
+            Positioned.fill(
+              bottom: 0,
+              left: 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      score,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                DecoratedBox(
-                  decoration: const BoxDecoration(color: Colors.white10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          title,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        score,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            title,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
