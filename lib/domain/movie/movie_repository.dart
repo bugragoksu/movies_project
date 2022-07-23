0477714019f495dc54be294bb0445ac84065e3e5
Buğra Göksu <bugra.goksu@parasut.com>
2022-07-23 13:16:50 +0300

    add abstract movie repository

---
+++ lib/domain/movie/movie_repository.dart
@@ -0,0 +1,11 @@
+import 'package:movie_challenge_project/domain/core/data_result.dart';
+import 'package:movie_challenge_project/domain/core/utils/pagination/paginated_response.dart';
+import 'package:movie_challenge_project/domain/core/utils/pagination/pagination_params.dart';
+import 'package:movie_challenge_project/domain/movie/entity/movie.dart';
+
+abstract class MovieRepository {
+  Future<DataResult<PaginatedResponse<List<Movie>>>> searchMovies({
+    required String query,
+    required PaginationParams paginationParams,
+  });
+}
