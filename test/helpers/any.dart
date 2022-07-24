5ce81e52caab70118b975d66362214d5e9fcc804
Buğra Göksu <bugra.goksu@parasut.com>
2022-07-24 14:29:22 +0300

    add data source tests

--- test/helpers/any.dart
+++ test/helpers/any.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:dio/dio.dart';
 import 'package:movie_challenge_project/domain/core/utils/pagination/meta_data.dart';
 import 'package:movie_challenge_project/domain/core/utils/pagination/pagination_params.dart';
 import 'package:movie_challenge_project/infrastructure/core/dto/paginated_response_dto.dart';
@@ -19,6 +20,15 @@ class Any {
   static bool boolean() => Random().nextBool();
 
   static DateTime dateTime() => DateTime.now().subtract(Duration(days: Random().nextInt(1000)));
+
+  static MetaData metaData({
+    int? currentPage,
+    int? totalPage,
+  }) =>
+      MetaData(
+        currentPage ?? Any.integer(),
+        totalPage ?? Any.integer(),
+      );
 
   static MovieItemDTO movieItemDTO({
     int? id,
@@ -85,5 +95,81 @@ class Any {
         pageNumber ?? Any.integer(),
       );
 
-  static ResponseException responseException() => ResponseException(statusCode: Any.integer(max: 500));
+  static ResponseException responseException() => ResponseException(statusCode: Any.integer(min: 400, max: 500));
+
+  static Response<Map<String, dynamic>> successResponseWithBody(Map<String, dynamic>? body) =>
+      Response<Map<String, dynamic>>(
+        statusCode: 200,
+        data: body,
+        requestOptions: RequestOptions(
+          path: Any.alphaString(10),
+        ),
+      );
+
+  static Response<T> errorResponse<T>({T? body, String? errorMessage}) => Response(
+        statusCode: 500,
+        data: body,
+        statusMessage: errorMessage ?? Any.alphaString(10),
+        requestOptions: RequestOptions(
+          path: Any.alphaString(10),
+        ),
+      );
+  static Map<String, dynamic> get searchMovieResultJson => {
+        "page": 1,
+        "results": [
+          {
+            "adult": false,
+            "backdrop_path": "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
+            "genre_ids": [12, 28, 878],
+            "id": 181808,
+            "original_language": "en",
+            "original_title": "Star Wars: The Last Jedi",
+            "overview":
+                "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+            "popularity": 78.994,
+            "poster_path": "/ySaaKHOLAQU5HoZqWmzDIj1VvZ1.jpg",
+            "release_date": "2017-12-13",
+            "title": "Star Wars: The Last Jedi",
+            "video": false,
+            "vote_average": 6.9,
+            "vote_count": 13185
+          },
+          {
+            "adult": false,
+            "backdrop_path": null,
+            "genre_ids": [],
+            "id": 693441,
+            "original_language": "en",
+            "original_title": "The Last Jedi: Meet the Porgs",
+            "overview":
+                "This piece explores Porg origins and inspiration, design, transition from sketches to final physical puppet, puppet control, sound effects, and digital support features.",
+            "popularity": 0.6,
+            "poster_path": null,
+            "release_date": "2018-03-27",
+            "title": "The Last Jedi: Meet the Porgs",
+            "video": true,
+            "vote_average": 0,
+            "vote_count": 0
+          },
+          {
+            "adult": false,
+            "backdrop_path": null,
+            "genre_ids": [878, 35],
+            "id": 441823,
+            "original_language": "en",
+            "original_title": "Star Worlds Episode XXXIVE=MC2: The Force Awakens The Last Jedi Who Went Rogue",
+            "overview":
+                "A Star Wars parody film by the filmmakers behind Scary Movie, Epic Movie, Meet the Spartans and Vampires Suck.",
+            "popularity": 1.204,
+            "poster_path": null,
+            "release_date": "",
+            "title": "Star Worlds Episode XXXIVE=MC2: The Force Awakens The Last Jedi Who Went Rogue",
+            "video": false,
+            "vote_average": 0,
+            "vote_count": 0
+          }
+        ],
+        "total_pages": 1,
+        "total_results": 3
+      };
 }
--- test/helpers/fakes.dart
+++ test/helpers/fakes.dart
@@ -1,4 +1,7 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:movie_challenge_project/infrastructure/movie/data-source/movie_data_source.dart';
+import 'package:movie_challenge_project/infrastructure/movie/service/movie_service.dart';
 
 class MockMovieDataSource extends Mock implements MovieDataSource {}
+
+class MockMovieService extends Mock implements MovieService {}
